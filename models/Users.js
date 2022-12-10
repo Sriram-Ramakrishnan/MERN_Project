5dddcdc0f8fc589f9d2c8dcb0a0c433211c95cf7
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-12-10 11:03:47 +0530

    User model created

---
+++ models/Users.js
@@ -0,0 +1,27 @@
+// Model Creation for user:
+const mongoose =  require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('user',UserSchema);
