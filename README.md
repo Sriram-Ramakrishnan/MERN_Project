129ba93a45e025e3c84bff8696552269146fea9d
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-12-12 22:41:04 +0530

    Middleware added to verify JWT

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Hello Everyone, I'm learning the MERN stack from this Udemy course down below, c
 ## Description
 * Will be pushing changes whenever I work on this project
 * Will be deploying it on Vercel soon
+
+## Working
+* For creating a user:
+    * First, a user model is created to store the user data we need
+    * Then, the data is sent via a POST request. express-validator is used to check various conditions
+    * After validation, we check for duplicate emails. 
+    * We then encrypt the password using Bcrypt
+    * JWT is created to log the user into the website
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 // Express router is required:
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-
-// @route       GET api/users
+const auth = require("../../middleware/auth");
+const User = require('../../models/Users');
+// @route       GET api/auth
 // @description Test route
 // @access      Public
-router.get('/', (req,res) => res.send("Auth route"));
+router.get('/',auth, 
+    async (req,res) => {
+    try {
+        // Middleware sends the user in the request, hence we can fetch it:
+        const user = await User.findById(req.user.id).select("-password");
+        res.json(user);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Server error");
+    }
+});
 
 module.exports = router;
