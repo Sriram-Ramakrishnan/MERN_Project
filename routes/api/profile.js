5d0a58494faf3f009a3dd29932920380f7dc346d
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-12-14 21:50:02 +0530

    Fixed null profile response

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -36,33 +36,22 @@ router.get('/me',auth,
 // @description Create/Update user's profile
 // @access      X Private X
 
-router.post('/',[auth, [
-    check('status', "Status is required").not().isEmpty(),
-    check('skills', "Skills required").not().isEmpty(),
-    ]],
-    async (req,res) =>{
+router.post('/', [
+    auth, 
+    [
+        check('status', 'Status reqd').not().isEmpty(), 
+        check('skills', 'Skills reqd').not().isEmpty()
+    ] 
+    ],
+    async (req,res)=>{
         const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(400).json({errors: errors.array()});
+        if(!errors.isEmpty()){ 
+            return res.status(400).json({ errors: errors.array()});
         }
-        // Get all the fields required:
-        const {
-            company,
-            website,
-            location,
-            bio,
-            status,
-            githubusername,
-            skills,
-            youtube,
-            twitter,
-            instagram,
-            linkedin,
-            facebook
-        } = req.body;
+        // Pull all fields out:
+        const {company, website, location, bio, status, githubusername, skills, youtube, facebook, twitter, instagram, linkedin } = req.body;
 
-        // Build Profile Object
-        const profileFields = {};
+        const profileFields = {}
         profileFields.user = req.user.id;
         if(company) profileFields.company = company;
         if(website) profileFields.website = website;
@@ -70,13 +59,12 @@ router.post('/',[auth, [
         if(bio) profileFields.bio = bio;
         if(status) profileFields.status = status;
         if(githubusername) profileFields.githubusername = githubusername;
-        
+
         if(skills){
-            profileFields.skills = skills.split(', ').map(skill => skill.trim());
+            profileFields.skills = skills.split(',').map(skill => skill.trim());
         }
         console.log(profileFields.skills);
-
-        // Initialise an array for social media:
+        // Social object:
         profileFields.social = {};
         if(youtube) profileFields.social.youtube = youtube;
         if(twitter) profileFields.social.twitter = twitter;
@@ -85,30 +73,29 @@ router.post('/',[auth, [
         if(facebook) profileFields.social.facebook = facebook;
 
         try {
-            // Find if it already exists:
-            let profile = Profile.findOne({user: req.user.id});
+            let profile = await Profile.findOne({ user: req.user.id });
             if(profile){
-                // Update the profile:
+                // Update:
                 profile = await Profile.findOneAndUpdate(
-                    {user: req.user.id}, 
+                    { user: req.user.id },
                     {$set: profileFields},
-                    {new: true});
-                console.log("Updated");
-                res.json(profile);
+                    { new: true }
+                    );
+                return res.json(profile);
             }
-            // Create a profile:
+
+            // Create:
             profile = new Profile(profileFields);
             await profile.save();
-            console.log("Created:");
-            return res.json(profile);
+            res.json(profile);
+            
         } catch (err) {
             console.error(err.message);
-            res.status(500).send("Server error");
-        }
-        res.send("Hello");
+            return res.status(500).send("SERVER ERROR");
 
-        
-    }
-);
+        }
+        // Send a response:
+        res.send("Profile response POST");
+});
 
 module.exports = router;
