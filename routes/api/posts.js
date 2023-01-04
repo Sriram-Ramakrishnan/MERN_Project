2e5c6958ed0d96e75d0409bb74da60f37698f3ab
Sriram Ramakrishnan <srirampro107@gmail.com>
2023-01-04 18:22:11 +0530

    Add and Delete comments functionality

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -167,5 +167,72 @@ async (req,res)=>{
     }
 });
 
+// @route       POST api/posts/comment/:id
+// @description Comment on a post
+// @access      Private
+router.post('/comment/:id', [auth , [
+    check('text', 'Text required').not().isEmpty()
+]],
+async (req,res) => {
+    const errors = validationResult(req);
+    // Check if errors array is not empty, return err response
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array()});
+    }
+    try {
+    // Gets the user but remove password as it is not needed:
+    const user = await User.findById(req.user.id).select('-password');
+    const post = await Post.findById(req.params.id);
+    const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+    }
+
+    post.comments.unshift(newComment);
+    await post.save();
+    res.json(post.comments);
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');  
+    }
+});
+
+// @route       DELETE api/posts/comment/:id/:comment_id
+// @description Delete Comments on a post
+// @access      Private
+
+router.delete('/comment/:id/:comment_id',auth,
+    async (req,res) =>{
+        try {
+            // Get post:
+            const post = await Post.findById(req.params.id);
+            // Get comment:
+            const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+            // Condition for comment exists:
+            if(!comment){
+                return res.status(404).json({msg: "Comment does not exist"});
+            }
+            if(comment.user.toString()!==req.user.id){
+                return res.status(401).json({msg: "User not authorized"});
+            }
+        
+            const removeIndex = post.comments
+            .map(comment => comment.user.toString()).indexOf(req.user.id);
+        post.comments.splice(removeIndex,1);
+
+        await post.save();
+        res.json(post.comments);
+
+        }catch(err){
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+    );
+
+
 
 module.exports = router;
