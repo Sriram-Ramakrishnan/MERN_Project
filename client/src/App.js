bea66a765d5aac5af691632ab709652ff538c502
Sriram Ramakrishnan <srirampro107@gmail.com>
2023-01-18 13:18:48 +0530

    feat: Added useState for register form

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import './App.css';
-import React,{ Fragment } from 'react';
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
-import Navbar from './components/layouts/Navbar';
-import Landing from './components/layouts/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
+import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Landing from "./components/layouts/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
- const App = () =>
- <Router>
-    <Navbar/>
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<Landing />} />
+    </Routes>
+    <section className="container">
       <Routes>
-        <Route exact path="/" element={<Landing/>} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
       </Routes>
-      <section className='container'>
-        <Routes>
-          <Route exact path="/login" element={<Login/>} />
-          <Route exact path="/register" element={<Register/>} />
-        </Routes>
-      </section>
-    </Router>
-;
-
+    </section>
+  </Router>
+);
 export default App;
--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <section className="landing">
@@ -11,13 +11,17 @@ const Landing = () => {
             other developers
           </p>
           <div className="buttons">
-            <Link to="/register" className="btn btn-primary">Sign Up</Link>
-            <Link to="/login" className="btn btn-light">Login</Link>
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Landing;
