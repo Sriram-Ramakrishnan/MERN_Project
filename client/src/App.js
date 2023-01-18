a51b736c51c1cee2df6fed1ff9f07f458c08b523
Sriram Ramakrishnan <srirampro107@gmail.com>
2023-01-18 12:17:18 +0530

    feat: Added routes for Login and Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import { Fragment } from 'react';
+import React,{ Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
-
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
  const App = () =>
-<Fragment> 
-  <Navbar/>
-  <Landing/>   
-  </Fragment>
+ <Router>
+    <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Landing/>} />
+      </Routes>
+      <section className='container'>
+        <Routes>
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/register" element={<Register/>} />
+        </Routes>
+      </section>
+    </Router>
 ;
 
 export default App;
